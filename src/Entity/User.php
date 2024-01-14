4e142d6793954e1cd53f341b4a4f099e1964e990
Lázaro <20268108+lazaropower@users.noreply.github.com>
2024-01-15 01:58:36 +0200

    Add password hashing and attributes for user fields

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,18 +2,42 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Enum\UserType;
 use App\Repository\UserRepository;
+use App\State\UserPasswordHasher;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ApiResource]
+#[ORM\Table(name: '`user`')]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Post(validationContext: ['groups' => ['Default', 'user:create']], processor: UserPasswordHasher::class),
+        new Post(processor: UserPasswordHasher::class),
+        new Get(),
+        new Put(processor: UserPasswordHasher::class),
+        new Patch(processor: UserPasswordHasher::class),
+        new Delete(),
+    ],
+    normalizationContext: ['groups' => ['user:read']],
+    denormalizationContext: ['groups' => ['user:create', 'user:update']],
+)]
+#[UniqueEntity('email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -21,39 +45,42 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255, unique: true)]
-    #[Assert\NotBlank]
-    #[Assert\Length(max: 255)]
-    private ?string $username = null;
-
-    #[ORM\Column(length: 255)]
-    #[Assert\NotBlank]
-    #[Assert\Length(max: 255)]
-    private ?string $password = null;
-
-    #[ORM\Column(length: 255, unique: true)]
     #[Assert\NotBlank]
     #[Assert\Email]
     #[Assert\Length(max: 255)]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[Assert\NotBlank(groups: ['user:create'])]
+    #[Groups(['user:create', 'user:update'])]
+    private ?string $plainPassword = null;
+
     #[ORM\Column(type: 'json')]
-    #[Assert\NotBlank]
+    #[Groups(['user:read'])]
     private array $roles = [];
 
-    #[ORM\Column(length: 255)]
-    #[Assert\NotBlank]
+    #[Assert\NotBlank(groups: ['user:create'])]
     #[Assert\Type(type: UserType::class)]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
+    #[ORM\Column(length: 255)]
     private ?UserType $userType = null;
 
-    #[ORM\Column(length: 255)]
-    #[Assert\NotBlank]
+
+    #[Assert\NotBlank(groups: ['user:create'])]
     #[Assert\DateTime(format: 'Y-m-d')]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
+    #[ORM\Column(length: 255)]
     private ?string $birthdate = null;
 
-    #[ORM\Column(length: 20, unique: true)]
-    #[Assert\NotBlank]
+
+    #[Assert\NotBlank(groups: ['user:update'])]
     #[Assert\Length(max: 20)]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
+    #[ORM\Column(length: 20, unique: true)]
     private ?string $idCard = null;
 
     #[ORM\ManyToMany(targetEntity: Ticket::class, mappedBy: 'user')]
@@ -69,18 +96,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this->id;
     }
 
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): static
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
     public function getPassword(): ?string
     {
         return $this->password;
@@ -89,6 +104,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPassword(string $password): static
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(?string $plainPassword): self
+    {
+        $this->plainPassword = $plainPassword;
 
         return $this;
     }
@@ -185,11 +212,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getUserIdentifier(): string
     {
-        return (string) $this->email;
+        return (string)$this->email;
     }
 
     public function eraseCredentials(): void
     {
-        // TODO: Implement eraseCredentials() method.
+        $this->plainPassword = null;
     }
 }
